bfa7f9108b4977cbd98dcc82e4256290ebab1275
Joao-Gontijo <jvgontijo166@gmail.com>
2022-03-28 15:46:03 -0300

    criando enum

---
+++ src/main/Prioridade.java
@@ -0,0 +1,5 @@
+package main;
+
+public enum Prioridade {
+	MIN, NORMAL, MAX;
+}
---
+++ src/main/Teste.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Teste {
+	public static void main(String[] args) {
+		
+		Prioridade pMin = Prioridade.MIN;
+		Prioridade pMax = Prioridade.MAX;
+		Prioridade pNormal = Prioridade.NORMAL;
+		
+		System.out.println(pMin + " " + pMax);
+		
+		System.out.println(pMax.compareTo(pNormal));
+		System.out.println(pMin.name());
+		System.out.println(pMax.ordinal());
+	}
+}
